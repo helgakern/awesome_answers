7768b0aea47cd487058c6cb54bf3091887ec7e26
Hano <hinsul3113@gmail.com>
2020-04-21 10:02:30 -0700

    remove column like_count from questions model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_21_165918) do
+ActiveRecord::Schema.define(version: 2020_04_21_170109) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,6 @@ ActiveRecord::Schema.define(version: 2020_04_21_165918) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "view_count"
-    t.integer "like_count"
   end
 
 end
