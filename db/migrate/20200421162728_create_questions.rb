f54dfb0d0b7806a95f77044a589a8edf4ab3eb14
Hano <hinsul3113@gmail.com>
2020-04-21 09:35:33 -0700

    Add a migration for questions

---
+++ db/migrate/20200421162728_create_questions.rb
@@ -0,0 +1,26 @@
+class CreateQuestions < ActiveRecord::Migration[6.0]
+  # This is the Question model. We generated this
+  # file with the command:
+  # rails g model question title:string body:text
+
+  # To run all you remaining(pending) migrations do:
+  # rails db:migrate
+
+  # To look at the status migrations (whether they are 
+  # active or not) do:
+  # rails db:migrate:status
+
+
+  # To reverse the last migration do:
+  # rails db:rollback
+  def change
+    create_table :questions do |t|
+      t.string :title
+      t.text :body
+
+      t.timestamps
+      # This will create two columns, "created_at",
+      # and "updated_at" which will auto update
+    end
+  end
+end
