7258d2d60981e18e60c6119eaa01534ecdf59533
Hano <hinsul3113@gmail.com>
2020-04-30 09:48:04 -0700

    Add tagging and tag models and associate them with questions

--- app/models/question.rb
+++ app/models/question.rb
@@ -22,6 +22,11 @@ class Question < ApplicationRecord
     # that no longer exist, likely leading to errors. 
     # Always set a dependent option to help maintain
     # referential integrity.
+    has_many :taggings, dependent: :destroy 
+    has_many :tags, through: :taggings#, source: :tag
+    # If the name of the association (i.e. tags) is the same as the
+    # source singularized (i.e. tag), then the 'source:' named
+    # argument can be omitted
 
     has_many :likes
     has_many :likers, through: :likes, source: :user
