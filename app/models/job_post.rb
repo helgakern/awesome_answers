01d227b134cec7df8083d53cebdf2f5c5584d060
helgakern <helgakern@gmail.com>
2020-05-05 09:39:01 -0700

    added test for jobposts

--- app/models/job_post.rb
+++ app/models/job_post.rb
@@ -1,6 +1,15 @@
 class JobPost < ApplicationRecord
   validates :title, presence: true, uniqueness: true
-  validates :description, presence: true, length: { minimum: 100 }
+  validates :description, presence: true, length: { minimum: 20 }
   validates :min_salary, numericality: { greater_than_or_equal_to: 30_000 }
   validates :location, presence: true
+
+  scope :search, -> (query) {
+    where("title ILIKE ? OR description ILIKE ? ", "%#{query}%", "%#{query}%")
+    # where("title ILIKE ? ", "%#{query}%") # if you want to search just by title
+  }
+  # equivalent to:
+  # def self.search(query)
+  #   where("title ILIKE ? OR description ILIKE ? ", "%#{query}%", "%#{query}%")
+  # end
 end
