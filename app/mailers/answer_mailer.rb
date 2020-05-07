a5af4bb89ca366b2e80336c84e67ae2bb1ebb38c
helgakern <helgakern@gmail.com>
2020-05-07 09:52:01 -0700

    created mailers views

--- app/mailers/answer_mailer.rb
+++ app/mailers/answer_mailer.rb
@@ -1,13 +1,25 @@
 class AnswerMailer < ApplicationMailer
 
-    def hello_world
-      mail(
-        to: "brandon@codecore.ca",
-        from: "info@awesome_answers.io",
-        cc: "tech_support@codecore.ca",
-        bcc: "some_more_people@example.ca",
-        subject: "Hello, World from Awesome Answers App!"
-      )
-    end
+  def hello_world
+    mail(
+      to: "brandon@codecore.ca",
+      from: "info@awesome_answers.io",
+      cc: "tech_support@codecore.ca",
+      bcc: "some_more_people@example.ca",
+      subject: "Hello, World from Awesome Answers App!"
+    )
+    # the mail method will automatically look for a template inside of `app/views/MAILER_NAME/METHOD_NAME`
+    # if you want to specify a template you can add the keys `tempalte_path` and `template_name` to the options hash
   end
-  
+
+  def notify_question_owner(answer)
+    @answer = answer
+    @question = answer.question
+    @question_owner = @question.user
+
+    mail(
+      to: @question_owner.email
+      subject: "#{answer.user.first_name} answerd your question!"
+    )
+  end
+end
--- spec/mailers/previews/answer_mailer_preview.rb
+++ spec/mailers/previews/answer_mailer_preview.rb
@@ -1,4 +1,9 @@
 # Preview all emails at http://localhost:3000/rails/mailers/answer_mailer
 class AnswerMailerPreview < ActionMailer::Preview
 
-end
+    # Rails 4+ allows us to preview what emails are going to look like via this class.
+    def hello_world
+      AnswerMailer.hello_world
+    end
+  end
+  
